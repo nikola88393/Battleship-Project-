6494e029cac3a5ae5192dc9f51c8bcf96c8c7e5a
Nikola <nikola88393@gmail.com>
2024-05-03 19:08:35 +0300

    Add hover effect to cell to indicate how big of a ship the player has to place

--- src/populateDOM.js
+++ src/populateDOM.js
@@ -15,30 +15,34 @@ export function drawGrid() {
     }
   }
 }
-export function markComputerShips(input) {
-  const containerComputer = document.querySelectorAll(".cellComputer");
-  const coordinates = input.slice();
-  while (coordinates.length > 0) {
-    if (coordinates[0].orientation === "h") {
-      for (
-        let i = coordinates[0].y;
-        i < coordinates[0].y + coordinates[0].shipSize;
-        i++
-      ) {
-        containerComputer[coordinates[0].x * 10 + i].classList.add("ship");
-      }
-    } else {
-      for (
-        let i = coordinates[0].x;
-        i < coordinates[0].x + coordinates[0].shipSize;
-        i++
-      ) {
-        containerComputer[i * 10 + coordinates[0].y].classList.add("ship");
-      }
-    }
-    coordinates.shift();
-  }
-}
+
+// The function below was used for showing the enemy ships while developing and testing the game
+
+// export function markComputerShips(input) {
+//   const containerComputer = document.querySelectorAll(".cellComputer");
+//   const coordinates = input.slice();
+//   while (coordinates.length > 0) {
+//     if (coordinates[0].orientation === "h") {
+//       for (
+//         let i = coordinates[0].y;
+//         i < coordinates[0].y + coordinates[0].shipSize;
+//         i++
+//       ) {
+//         containerComputer[coordinates[0].x * 10 + i].classList.add("ship");
+//       }
+//     } else {
+//       for (
+//         let i = coordinates[0].x;
+//         i < coordinates[0].x + coordinates[0].shipSize;
+//         i++
+//       ) {
+//         containerComputer[i * 10 + coordinates[0].y].classList.add("ship");
+//       }
+//     }
+//     coordinates.shift();
+//   }
+// }
+
 export function switchOrientation() {
   const btn = document.getElementById("switchOrientation");
 
@@ -53,6 +57,7 @@ export function updateErrorDisplay(err = null) {
 
 export function placeShips(player) {
   const cells = document.querySelectorAll(".cellPlayer");
+  const shipsSizes = [5, 4, 3, 3, 2];
 
   cells.forEach((cell) => {
     cell.addEventListener("click", () => {
@@ -74,6 +79,71 @@ export function placeShips(player) {
             const cellWithShip = document.getElementById(`player${i}${j}`);
             cellWithShip.classList.add("ship");
           }
+        }
+        shipsSizes.shift();
+      }
+    });
+
+    cell.addEventListener("mouseover", () => {
+      const index = Array.from(cell.parentElement.children).indexOf(cell);
+      const { orientation } =
+        document.getElementById("switchOrientation").dataset;
+      const cellsToHighlight = [];
+      if (orientation === "h") {
+        if ((index % 10) + shipsSizes[0] <= 10) {
+          for (let i = 0; i < shipsSizes[0]; i++) {
+            const nextCell = cell.parentElement.children[index + i];
+            cellsToHighlight.push(nextCell);
+          }
+        }
+
+        if (cellsToHighlight.length === shipsSizes[0]) {
+          cellsToHighlight.forEach((element) => element.classList.add("hover"));
+        }
+      } else {
+        if (Math.floor(index / 10) + shipsSizes[0] <= 10) {
+          for (let i = 0; i < shipsSizes[0]; i++) {
+            const nextCell = cell.parentElement.children[index + 10 * i];
+            cellsToHighlight.push(nextCell);
+          }
+        }
+
+        if (cellsToHighlight.length === shipsSizes[0]) {
+          cellsToHighlight.forEach((element) => element.classList.add("hover"));
+        }
+      }
+    });
+
+    cell.addEventListener("mouseout", () => {
+      const index = Array.from(cell.parentElement.children).indexOf(cell);
+      const { orientation } =
+        document.getElementById("switchOrientation").dataset;
+      const cellsToUnhighlight = [];
+      if (orientation === "h") {
+        if ((index % 10) + shipsSizes[0] <= 10) {
+          for (let i = 0; i < shipsSizes[0]; i++) {
+            const nextCell = cell.parentElement.children[index + i];
+            cellsToUnhighlight.push(nextCell);
+          }
+        }
+
+        if (cellsToUnhighlight.length === shipsSizes[0]) {
+          cellsToUnhighlight.forEach((element) =>
+            element.classList.remove("hover")
+          );
+        }
+      } else {
+        if (Math.floor(index / 10) + shipsSizes[0] <= 10) {
+          for (let i = 0; i < shipsSizes[0]; i++) {
+            const nextCell = cell.parentElement.children[index + 10 * i];
+            cellsToUnhighlight.push(nextCell);
+          }
+        }
+
+        if (cellsToUnhighlight.length === shipsSizes[0]) {
+          cellsToUnhighlight.forEach((element) =>
+            element.classList.remove("hover")
+          );
         }
       }
     });
@@ -126,6 +196,14 @@ export const addClassHitToComputerShip = (n) => {
   const computerGrid = document.querySelector(`.cellComputer:nth-child(${n})`);
   computerGrid.classList.add("ship");
 };
+
+// const playerCells = document.querySelectorAll(".cellPlayer");
+// playerCells.forEach((cell) => {
+//   cell.addEventListener("mouseover", () => {
+//     cell.classList.add("hover");
+//   });
+// });
+
 const btn = document.getElementById("switchOrientation");
 btn.addEventListener("click", switchOrientation);
 // export function renderPlayerBoard(){}
